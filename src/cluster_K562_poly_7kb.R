3c836efb16200275462bc774c2a7c1692d75ca88
andreaskapou <kapouranis.andreas@gmail.com>
2016-03-07 19:22:34 +0000

    Update parameters for polynomial basis

--- src/cluster_K562_poly_7kb.R
+++ src/cluster_K562_poly_7kb.R
@@ -67,12 +67,12 @@ seed        <- 1234
 K           <- 5
 pi_k        <- NULL
 w           <- NULL
-basis       <- polynomial.object(M = 5)
+basis       <- polynomial.object(M = 6)
 em_max_iter <- 20
 epsilon_conv <- 1e-4
 opt_method  <- "CG"
 opt_itnmax  <- 50
-init_opt_itnmax <- 50
+init_opt_itnmax <- 100
 is_parallel <- TRUE
 no_cores    <- 5
 is_verbose  <- TRUE
